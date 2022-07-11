37412194d46c280fbb9969e93788b0a10f0c60e4
Nicolin Chen <nicolinc@nvidia.com>
2022-07-11 14:39:08 -0700

    vfio: Set up nested stage mappings
    In IOMMUFD based nesting mode, legacy vfio_iommu_map_notify
    will not be used as there is no "caching" mode and we do not
    trap on map.
    
    On Intel, vfio_iommu_map_notify was used to DMA map the RAM
    through the host single stage.
    
    With nested mode, we need to setup the stage 2 and the stage 1
    separately. This patch introduces a prereg_listener to setup
    the stage 2 mapping.
    
    The stage 1 mapping, owned by the guest, is passed to the host
    via the IOMMUFD APIs.
    
    Change-Id: Ic4372284c81c532a2e49eb6bbf3ae6bd88a5857d
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    
    ---
    nicoinc: only add prereg_listener for stage-2 mappings.
             FIXME, pass "nested-smmu" to "bool nested".
    
    v9 -> v10:
    - report error when the guest does not use range invalidation
    
    v7 -> v8:
    - properly handle new IOMMUTLBEntry fields and especially
    propagate DOMAIN and PASID based invalidations
    
    v6 -> v7:
    - remove PASID based invalidation
    
    v5 -> v6:
    - add error_report_err()
    - remove the abort in case of nested stage case
    
    v4 -> v5:
    - use VFIO_IOMMU_SET_PASID_TABLE
    - use PCIPASIDOps for config notification
    
    v3 -> v4:
    - use iommu_inv_pasid_info for ASID invalidation
    
    v2 -> v3:
    - use VFIO_IOMMU_ATTACH_PASID_TABLE
    - new user API
    - handle leaf
    
    v1 -> v2:
    - adapt to uapi changes
    - pass the asid
    - pass IOMMU_NOTIFIER_S1_CFG when initializing the config notifier

--- hw/vfio/as.c
+++ hw/vfio/as.c
@@ -651,7 +651,10 @@ static void vfio_container_region_add(VFIOContainer *container,
             goto fail;
         }
         QLIST_INSERT_HEAD(&container->giommu_list, giommu, giommu_next);
-        memory_region_iommu_replay(giommu->iommu_mr, &giommu->n);
+
+        if (!container->nested) { /* FIXME */
+            memory_region_iommu_replay(giommu->iommu_mr, &giommu->n);
+        }
 
         return;
     }
@@ -979,6 +982,40 @@ VFIOAddressSpace *vfio_get_address_space(AddressSpace *as)
     return space;
 }
 
+static void vfio_prereg_listener_region_add(MemoryListener *listener,
+                                            MemoryRegionSection *section)
+{
+    VFIOContainer *container =
+        container_of(listener, VFIOContainer, prereg_listener);
+    Error *err = NULL;
+
+    if (!memory_region_is_ram(section->mr)) {
+        return;
+    }
+
+    vfio_dma_map_ram_section(container, NULL, section, &err);
+    if (err) {
+        error_report_err(err);
+    }
+}
+static void vfio_prereg_listener_region_del(MemoryListener *listener,
+                                            MemoryRegionSection *section)
+{
+    VFIOContainer *container =
+        container_of(listener, VFIOContainer, prereg_listener);
+
+    if (!memory_region_is_ram(section->mr)) {
+        return;
+    }
+
+    vfio_dma_unmap_ram_section(container, section);
+}
+
+static MemoryListener vfio_memory_prereg_listener = {
+    .region_add = vfio_prereg_listener_region_add,
+    .region_del = vfio_prereg_listener_region_del,
+};
+
 void vfio_as_add_container(VFIOAddressSpace *space,
                            VFIOContainer *container)
 {
@@ -992,6 +1029,12 @@ void vfio_as_add_container(VFIOAddressSpace *space,
     space->listener = vfio_memory_listener;
     memory_listener_register(&space->listener, space->as);
     space->listener_initialized = true;
+
+    if (container->nested) { /* FIXME */
+        container->prereg_listener = vfio_memory_prereg_listener;
+        memory_listener_register(&container->prereg_listener,
+                                 &address_space_memory);
+    }
 }
 
 void vfio_as_del_container(VFIOAddressSpace *space,
