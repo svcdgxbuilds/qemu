c05b773cf9c03abda4a5cd553499396572013273
Nicolin Chen <nicolinc@nvidia.com>
2023-11-09 15:07:42 -0800

    Fix uapi header to 6.7-rc1
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- linux-headers/linux/iommufd.h
+++ linux-headers/linux/iommufd.h
@@ -47,9 +47,9 @@ enum {
 	IOMMUFD_CMD_VFIO_IOAS,
 	IOMMUFD_CMD_HWPT_ALLOC,
 	IOMMUFD_CMD_GET_HW_INFO,
+	IOMMUFD_CMD_HWPT_SET_DIRTY_TRACKING,
+	IOMMUFD_CMD_HWPT_GET_DIRTY_BITMAP,
 	IOMMUFD_CMD_HWPT_INVALIDATE,
-	IOMMUFD_CMD_SET_DEV_DATA,
-	IOMMUFD_CMD_UNSET_DEV_DATA,
 };
 
 /**
@@ -352,12 +352,14 @@ struct iommu_vfio_ioas {
 
 /**
  * enum iommufd_hwpt_alloc_flags - Flags for HWPT allocation
- * @IOMMU_HWPT_ALLOC_NEST_PARENT: If set, allocate a domain which can serve
- *                                as the parent domain in the nesting
- *                                configuration.
+ * @IOMMU_HWPT_ALLOC_NEST_PARENT: If set, allocate a HWPT that can serve as
+ *                                the parent HWPT in a nesting configuration.
+ * @IOMMU_HWPT_ALLOC_DIRTY_TRACKING: Dirty tracking support for device IOMMU is
+ *                                   enforced on device attachment
  */
 enum iommufd_hwpt_alloc_flags {
 	IOMMU_HWPT_ALLOC_NEST_PARENT = 1 << 0,
+	IOMMU_HWPT_ALLOC_DIRTY_TRACKING = 1 << 1,
 };
 
 /**
@@ -375,7 +377,7 @@ enum iommu_hwpt_vtd_s1_flags {
 
 /**
  * struct iommu_hwpt_vtd_s1 - Intel VT-d stage-1 page table
- *                            info (IOMMU_HWPT_TYPE_VTD_S1)
+ *                            info (IOMMU_HWPT_DATA_VTD_S1)
  * @flags: Combination of enum iommu_hwpt_vtd_s1_flags
  * @pgtbl_addr: The base address of the stage-1 page table.
  * @addr_width: The address width of the stage-1 page table
@@ -389,50 +391,13 @@ struct iommu_hwpt_vtd_s1 {
 };
 
 /**
- * struct iommu_hwpt_arm_smmuv3 - ARM SMMUv3 specific translation table info
- *                                (IOMMU_HWPT_TYPE_ARM_SMMUV3)
- *
- * @flags: Translation table entry attributes
- * @ste_len: Length of the user Stream Table Entry
- * @ste_uptr: User pointer to a user Stream Table Entry
- * @event_len: Length of the returning event
- * @out_event_uptr: User pointer to a returning event, to report a C_BAD_STE
- *                  upon an STE configuration failure
- *
- * ARM SMMUv3 specific data to create page tables for a nested configuration.
- *
- * For a nested stage-1 translation table allocation, kernel will read all the
- * information of a user space stage-1 Context Descriptor table from the given
- * user space Stream Table Entry pointed by @ste_uptr. The @event_len and the
- * @out_event_uptr in pair are optional. If they are both provided, kernel will
- * report an STE error to the memory location pointed by @out_event_uptr, when
- * the allocation fails due to some problem in the user space STE.
- *
- * As long as the SMMUv3 hardware supports a stage-1 page table, the default
- * allocation of a page table in the kernel is always for a stage-1 type. So,
- * this data structure can be also used to allocate a kernel-managed stage-2
- * translation table, by setting IOMMU_SMMUV3_FLAG_S2 in the @flags, in which
- * case only this flag matters and the kernel will ignore all other inputs.
+ * enum iommu_hwpt_data_type - IOMMU HWPT Data Type
+ * @IOMMU_HWPT_DATA_NONE: no data
+ * @IOMMU_HWPT_DATA_VTD_S1: Intel VT-d stage-1 page table
  */
-struct iommu_hwpt_arm_smmuv3 {
-#define IOMMU_SMMUV3_FLAG_S2	(1 << 0) /* if unset, stage-1 */
-	__aligned_u64 flags;
-	__aligned_u64 ste_len;
-	__aligned_u64 ste_uptr;
-	__aligned_u64 event_len;
-	__aligned_u64 out_event_uptr;
-};
-
-/**
- * enum iommu_hwpt_type - IOMMU HWPT Type
- * @IOMMU_HWPT_TYPE_DEFAULT: default
- * @IOMMU_HWPT_TYPE_VTD_S1: Intel VT-d stage-1 page table
- * @IOMMU_HWPT_TYPE_ARM_SMMUV3: ARM SMMUv3 Translation table
- */
-enum iommu_hwpt_type {
-	IOMMU_HWPT_TYPE_DEFAULT,
-	IOMMU_HWPT_TYPE_VTD_S1,
-	IOMMU_HWPT_TYPE_ARM_SMMUV3,
+enum iommu_hwpt_data_type {
+	IOMMU_HWPT_DATA_NONE,
+	IOMMU_HWPT_DATA_VTD_S1,
 };
 
 /**
@@ -443,7 +408,7 @@ enum iommu_hwpt_type {
  * @pt_id: The IOAS or HWPT to connect this HWPT to
  * @out_hwpt_id: The ID of the new HWPT
  * @__reserved: Must be 0
- * @hwpt_type: One of enum iommu_hwpt_type
+ * @data_type: One of enum iommu_hwpt_data_type
  * @data_len: Length of the type specific data
  * @data_uptr: User pointer to the type specific data
  *
@@ -452,17 +417,17 @@ enum iommu_hwpt_type {
  * underlying iommu driver's iommu_domain kernel object.
  *
  * A kernel-managed HWPT will be created with the mappings from the given
- * IOAS via the @pt_id. The @hwpt_type for this allocation can be set to
- * either IOMMU_HWPT_TYPE_DEFAULT or a pre-defined type corresponding to
- * an I/O page table type supported by the underlying IOMMU hardware.
+ * IOAS via the @pt_id. The @data_type for this allocation must be set to
+ * IOMMU_HWPT_DATA_NONE. The HWPT can be allocated as a parent HWPT for a
+ * nesting configuration by passing IOMMU_HWPT_ALLOC_NEST_PARENT via @flags.
  *
- * A user-managed HWPT will be created from a given parent HWPT via the
+ * A user-managed nested HWPT will be created from a given parent HWPT via
  * @pt_id, in which the parent HWPT must be allocated previously via the
- * same ioctl from a given IOAS (@pt_id). In this case, the @hwpt_type
+ * same ioctl from a given IOAS (@pt_id). In this case, the @data_type
  * must be set to a pre-defined type corresponding to an I/O page table
  * type supported by the underlying IOMMU hardware.
  *
- * If the @hwpt_type is set to IOMMU_HWPT_TYPE_DEFAULT, @data_len and
+ * If the @data_type is set to IOMMU_HWPT_DATA_NONE, @data_len and
  * @data_uptr should be zero. Otherwise, both @data_len and @data_uptr
  * must be given.
  */
@@ -473,7 +438,7 @@ struct iommu_hwpt_alloc {
 	__u32 pt_id;
 	__u32 out_hwpt_id;
 	__u32 __reserved;
-	__u32 hwpt_type;
+	__u32 data_type;
 	__u32 data_len;
 	__aligned_u64 data_uptr;
 };
@@ -481,9 +446,9 @@ struct iommu_hwpt_alloc {
 
 /**
  * enum iommu_hw_info_vtd_flags - Flags for VT-d hw_info
- * @IOMMU_HW_INFO_VTD_ERRATA_772415_SPR17: If set, disallow nesting on domains
- *                                   with read-only mapping.
- *                                   https://www.intel.com/content/www/us/en/content-details/772415/content-details.html
+ * @IOMMU_HW_INFO_VTD_ERRATA_772415_SPR17: If set, disallow read-only mappings
+ *                                         on a nested_parent domain.
+ *                                         https://www.intel.com/content/www/us/en/content-details/772415/content-details.html
  */
 enum iommu_hw_info_vtd_flags {
 	IOMMU_HW_INFO_VTD_ERRATA_772415_SPR17 = 1 << 0,
@@ -549,6 +514,20 @@ enum iommu_hw_info_type {
 };
 
 /**
+ * enum iommufd_hw_capabilities
+ * @IOMMU_HW_CAP_DIRTY_TRACKING: IOMMU hardware support for dirty tracking
+ *                               If available, it means the following APIs
+ *                               are supported:
+ *
+ *                                   IOMMU_HWPT_GET_DIRTY_BITMAP
+ *                                   IOMMU_HWPT_SET_DIRTY_TRACKING
+ *
+ */
+enum iommufd_hw_capabilities {
+	IOMMU_HW_CAP_DIRTY_TRACKING = 1 << 0,
+};
+
+/**
  * struct iommu_hw_info - ioctl(IOMMU_GET_HW_INFO)
  * @size: sizeof(struct iommu_hw_info)
  * @flags: Must be 0
@@ -559,6 +538,8 @@ enum iommu_hw_info_type {
  *             the iommu type specific hardware information data
  * @out_data_type: Output the iommu hardware info type as defined in the enum
  *                 iommu_hw_info_type.
+ * @out_capabilities: Output the generic iommu capability info type as defined
+ *                    in the enum iommu_hw_capabilities.
  * @__reserved: Must be 0
  *
  * Query an iommu type specific hardware information data from an iommu behind
@@ -583,35 +564,111 @@ struct iommu_hw_info {
 	__aligned_u64 data_uptr;
 	__u32 out_data_type;
 	__u32 __reserved;
+	__aligned_u64 out_capabilities;
 };
 #define IOMMU_GET_HW_INFO _IO(IOMMUFD_TYPE, IOMMUFD_CMD_GET_HW_INFO)
+
+/*
+ * enum iommufd_hwpt_set_dirty_tracking_flags - Flags for steering dirty
+ *                                              tracking
+ * @IOMMU_HWPT_DIRTY_TRACKING_ENABLE: Enable dirty tracking
+ */
+enum iommufd_hwpt_set_dirty_tracking_flags {
+	IOMMU_HWPT_DIRTY_TRACKING_ENABLE = 1,
+};
+
+/**
+ * struct iommu_hwpt_set_dirty_tracking - ioctl(IOMMU_HWPT_SET_DIRTY_TRACKING)
+ * @size: sizeof(struct iommu_hwpt_set_dirty_tracking)
+ * @flags: Combination of enum iommufd_hwpt_set_dirty_tracking_flags
+ * @hwpt_id: HW pagetable ID that represents the IOMMU domain
+ * @__reserved: Must be 0
+ *
+ * Toggle dirty tracking on an HW pagetable.
+ */
+struct iommu_hwpt_set_dirty_tracking {
+	__u32 size;
+	__u32 flags;
+	__u32 hwpt_id;
+	__u32 __reserved;
+};
+#define IOMMU_HWPT_SET_DIRTY_TRACKING _IO(IOMMUFD_TYPE, \
+					  IOMMUFD_CMD_HWPT_SET_DIRTY_TRACKING)
+
+/**
+ * enum iommufd_hwpt_get_dirty_bitmap_flags - Flags for getting dirty bits
+ * @IOMMU_HWPT_GET_DIRTY_BITMAP_NO_CLEAR: Just read the PTEs without clearing
+ *                                        any dirty bits metadata. This flag
+ *                                        can be passed in the expectation
+ *                                        where the next operation is an unmap
+ *                                        of the same IOVA range.
+ *
+ */
+enum iommufd_hwpt_get_dirty_bitmap_flags {
+	IOMMU_HWPT_GET_DIRTY_BITMAP_NO_CLEAR = 1,
+};
+
+/**
+ * struct iommu_hwpt_get_dirty_bitmap - ioctl(IOMMU_HWPT_GET_DIRTY_BITMAP)
+ * @size: sizeof(struct iommu_hwpt_get_dirty_bitmap)
+ * @hwpt_id: HW pagetable ID that represents the IOMMU domain
+ * @flags: Combination of enum iommufd_hwpt_get_dirty_bitmap_flags
+ * @__reserved: Must be 0
+ * @iova: base IOVA of the bitmap first bit
+ * @length: IOVA range size
+ * @page_size: page size granularity of each bit in the bitmap
+ * @data: bitmap where to set the dirty bits. The bitmap bits each
+ *        represent a page_size which you deviate from an arbitrary iova.
+ *
+ * Checking a given IOVA is dirty:
+ *
+ *  data[(iova / page_size) / 64] & (1ULL << ((iova / page_size) % 64))
+ *
+ * Walk the IOMMU pagetables for a given IOVA range to return a bitmap
+ * with the dirty IOVAs. In doing so it will also by default clear any
+ * dirty bit metadata set in the IOPTE.
+ */
+struct iommu_hwpt_get_dirty_bitmap {
+	__u32 size;
+	__u32 hwpt_id;
+	__u32 flags;
+	__u32 __reserved;
+	__aligned_u64 iova;
+	__aligned_u64 length;
+	__aligned_u64 page_size;
+	__aligned_u64 data;
+};
+#define IOMMU_HWPT_GET_DIRTY_BITMAP _IO(IOMMUFD_TYPE, \
+					IOMMUFD_CMD_HWPT_GET_DIRTY_BITMAP)
 
 /**
  * enum iommu_hwpt_vtd_s1_invalidate_flags - Flags for Intel VT-d
  *                                           stage-1 cache invalidation
- * @IOMMU_VTD_QI_FLAGS_LEAF: The LEAF flag indicates whether only the
- *                           leaf PTE caching needs to be invalidated
- *                           and other paging structure caches can be
- *                           preserved.
+ * @IOMMU_VTD_INV_FLAGS_LEAF: The LEAF flag indicates whether only the
+ *                            leaf PTE caching needs to be invalidated
+ *                            and other paging structure caches can be
+ *                            preserved.
  */
 enum iommu_hwpt_vtd_s1_invalidate_flags {
-	IOMMU_VTD_QI_FLAGS_LEAF = 1 << 0,
+	IOMMU_VTD_INV_FLAGS_LEAF = 1 << 0,
 };
 
 /**
  * struct iommu_hwpt_vtd_s1_invalidate - Intel VT-d cache invalidation
- *                                       (IOMMU_HWPT_TYPE_VTD_S1)
- * @addr: The start address of the addresses to be invalidated.
+ *                                       (IOMMU_HWPT_DATA_VTD_S1)
+ * @addr: The start address of the addresses to be invalidated. It needs
+ *        to be 4KB aligned.
  * @npages: Number of contiguous 4K pages to be invalidated.
  * @flags: Combination of enum iommu_hwpt_vtd_s1_invalidate_flags
  * @__reserved: Must be 0
  *
  * The Intel VT-d specific invalidation data for user-managed stage-1 cache
- * invalidation under nested translation. Userspace uses this structure to
- * tell host about the impacted caches after modifying the stage-1 page table.
+ * invalidation in nested translation. Userspace uses this structure to
+ * tell the impacted cache scope after modifying the stage-1 page table.
  *
  * Invalidating all the caches related to the page table by setting @addr
- * to be 0 and @npages to be __aligned_u64(-1).
+ * to be 0 and @npages to be __aligned_u64(-1). This includes the
+ * corresponding device-TLB if ATS is enabled on the attached devices.
  */
 struct iommu_hwpt_vtd_s1_invalidate {
 	__aligned_u64 addr;
@@ -621,36 +678,23 @@ struct iommu_hwpt_vtd_s1_invalidate {
 };
 
 /**
- * struct iommu_hwpt_arm_smmuv3_invalidate - ARM SMMUv3 cahce invalidation
- *                                           (IOMMU_HWPT_TYPE_ARM_SMMUV3)
- * @cmd: 128-bit cache invalidation command that runs in SMMU CMDQ
- *
- * Supported command list:
- *     CMDQ_OP_TLBI_NSNH_ALL
- *     CMDQ_OP_TLBI_NH_VA
- *     CMDQ_OP_TLBI_NH_VAA
- *     CMDQ_OP_TLBI_NH_ALL
- *     CMDQ_OP_TLBI_NH_ASID
- *     CMDQ_OP_ATC_INV
- *     CMDQ_OP_CFGI_CD
- *     CMDQ_OP_CFGI_CD_ALL
- */
-struct iommu_hwpt_arm_smmuv3_invalidate {
-	__aligned_u64 cmd[2];
-};
-
-/**
  * struct iommu_hwpt_invalidate - ioctl(IOMMU_HWPT_INVALIDATE)
  * @size: sizeof(struct iommu_hwpt_invalidate)
- * @hwpt_id: HWPT ID of target hardware page table for cache invalidation
+ * @hwpt_id: HWPT ID of a nested HWPT for cache invalidation
  * @reqs_uptr: User pointer to an array having @req_num of cache invalidation
  *             requests. The request entries in the array are of fixed width
  *             @req_len, and contain a user data structure for invalidation
  *             request specific to the given hardware page table.
+ * @req_type: One of enum iommu_hwpt_data_type, defining the data type of all
+ *            the entries in the invalidation request array. It should suit
+ *            with the data_type passed per the allocation of the hwpt pointed
+ *            by @hwpt_id.
  * @req_len: Length (in bytes) of a request entry in the request array
  * @req_num: Input the number of cache invalidation requests in the array.
  *           Output the number of requests successfully handled by kernel.
- * @out_driver_error_code: Report a driver speicifc error code upon failure
+ * @out_driver_error_code: Report a driver speicifc error code upon failure.
+ *                         It's optional, driver has a choice to fill it or
+ *                         not.
  *
  * Invalidate the iommu cache for user-managed page table. Modifications on a
  * user-managed page table should be followed by this operation to sync cache.
@@ -661,51 +705,10 @@ struct iommu_hwpt_invalidate {
 	__u32 size;
 	__u32 hwpt_id;
 	__aligned_u64 reqs_uptr;
+	__u32 req_type;
 	__u32 req_len;
 	__u32 req_num;
 	__u32 out_driver_error_code;
 };
 #define IOMMU_HWPT_INVALIDATE _IO(IOMMUFD_TYPE, IOMMUFD_CMD_HWPT_INVALIDATE)
-
-/**
- * struct iommu_dev_data_arm_smmuv3 - ARM SMMUv3 specific device data
- * @sid: The Stream ID that is assigned in the user space
- *
- * The SMMUv3 specific user space data for a device that is behind an SMMU HW.
- * The guest-level user data should be linked to the host-level kernel data,
- * which will be used by user space cache invalidation commands.
- */
-struct iommu_dev_data_arm_smmuv3 {
-	__u32 sid;
-};
-
-/**
- * struct iommu_set_dev_data - ioctl(IOMMU_SET_DEV_DATA)
- * @size: sizeof(struct iommu_set_dev_data)
- * @dev_id: The device to set an iommu specific device data
- * @data_uptr: User pointer of the device user data
- * @data_len: Length of the device user data
- *
- * The device data must be unset using ioctl(IOMMU_UNSET_DEV_DATA), before
- * another ioctl(IOMMU_SET_DEV_DATA) call or before the device itself gets
- * unbind'd from the iommufd context.
- */
-struct iommu_set_dev_data {
-	__u32 size;
-	__u32 dev_id;
-	__aligned_u64 data_uptr;
-	__u32 data_len;
-};
-#define IOMMU_SET_DEV_DATA _IO(IOMMUFD_TYPE, IOMMUFD_CMD_SET_DEV_DATA)
-
-/**
- * struct iommu_unset_dev_data - ioctl(IOMMU_UNSET_DEV_DATA)
- * @size: sizeof(struct iommu_unset_dev_data)
- * @dev_id: The device to unset its device user data
- */
-struct iommu_unset_dev_data {
-	__u32 size;
-	__u32 dev_id;
-};
-#define IOMMU_UNSET_DEV_DATA _IO(IOMMUFD_TYPE, IOMMUFD_CMD_UNSET_DEV_DATA)
 #endif
